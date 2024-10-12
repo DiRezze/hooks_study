41a8671dadecb61aae808e3eb4042e680f58e1c6
Gabriel Bortoleti Di Rezze <bortoletigabriel.drz@gmail.com>
2024-10-12 16:58:20 -0300

    fix: navbar resposiviness

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import reactLogo from '../assets/react.svg';
 import './styles/navbar.css'
 
 const Navbar = () => {
+    const [width, setWidth] = useState(1000);
+    
+    useEffect(()=>{
+
+        const handleResize = () => {
+            const newWidth = window.innerWidth;
+            setWidth(newWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return ()=> {
+            window.removeEventListener('resize', handleResize)
+
+        }
+    }, []);
     return(
         <nav className="nav-bar">
             <div className="brand-container">
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" className="brand-link">
                     <img src={reactLogo} className="logo react brand-icon" />
                 </a>
             </div>
-            <ul className="links-list">
-                <li>Introdução</li>
-                <li>useState</li>
-                <li>useReducer</li>
-                <li>useEffect</li>
-            </ul>
+            {
+                width<=400 ?
+                null : 
+                <ul className="links-list">
+                    <li>Introdução</li>
+                    <li>useState</li>
+                    <li>useReducer</li>
+                    <li>useEffect</li>
+                </ul>
+            }
         </nav>
     );
 }
